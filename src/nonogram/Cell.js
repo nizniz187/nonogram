e6f7af523b123142ff1caca1055845f7f85a0aa7
Niz <nizniz187@gmail.com>
2020-04-19 15:44:22 +0800

    Use constat for defined bit value & cell class name.

--- src/nonogram/Cell.js
+++ src/nonogram/Cell.js
@@ -7,23 +7,31 @@ import React from 'react';
 class Cell extends React.Component {
   render() {
     return (
-      <div className={`nonogram-cell ${this.bitClassName}`}
+      <div className={`nonogram-cell ${this.cellClassName}`}
         data-row-index={this.props.rowIndex}
         data-col-index={this.props.colIndex}
       />
     );
   }
 
-  get bitClassName() {
+  get cellClassName() {
     switch(this.props.userBit) {
-      case 1:
-        return 'checked';
-      case -1:
-        return 'excluded';
+      case BIT_VALUE_CHECKED:
+        return CELL_CLASS_CHECKED;
+      case BIT_VALUE_EXCLUDED:
+        return CELL_CLASS_EXCLUDED;
       default:
-        return '';
+        return CELL_CLASS_UNCHECKED;
     }
   }
 }
+
+export const BIT_VALUE_CHECKED = 1;
+export const BIT_VALUE_UNCHECKED = 0;
+export const BIT_VALUE_EXCLUDED = -1;
+
+const CELL_CLASS_CHECKED = 'checked';
+const CELL_CLASS_EXCLUDED = 'excluded';
+const CELL_CLASS_UNCHECKED = '';
 
 export default Cell;
